5504eb002baa48d9540f3778fa1dab971d1781b8
Megan Kudzia <kudzia@mail.lib.msu.edu>
2015-11-25 11:18:28 -0500

    more updates to try and fix write to file failure

--- transcription/transcript_parsing.py
+++ transcription/transcript_parsing.py
@@ -13,27 +13,27 @@ def scrape_text(in_file, out_file):
 	    	if not any(char.isdigit() for char in line):
 	    		# print(line)
 	    		# maybe I need to write to the output file here, instead of in another function?
-	    		line.append
+	    		# line.append
 	    		return lines
 
-# commented out for testing purposes
 
-# def scrape_timestamps(in_file, out_file):
-#     # line = []
-#     with open(in_file, "r") as f:
-#         for line in f:
-#             if any(char.isdigit() for char in line):
-#                 print(line)
-#                 # open(out_file_time, "w")
-#                 # out_file_time.append(line)
+def scrape_timestamps(in_file, out_file):
+    lines = []
+    with open(in_file, "r") as f:
+        for line in f:
+            if any(char.isdigit() for char in line):
+                # print(line)
+                return lines
+                # open(out_file_time, "w")
+                # out_file_time.append(line)
 
 
 
 def store_timestamps(timestamps, out):
-    with open(out_file, "w") as f:
-        for lines in timestamps:
-            f.write(lines)
+	for lines in timestamps:
+		with open(out_file_time, "w") as f:
+			f.write(lines)
 
 scrape_text(curr_file, out_file_trns)
 scrape_timestamps(curr_file, out_file_time)
-# store_timestamps(scrape_timestamps, out_file)
+store_timestamps(scrape_timestamps, out_file_time)
