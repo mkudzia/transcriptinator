97e2a5432eb27e10f79566817c0b87af7d970887
Megan Kudzia <kudzia@mail.lib.msu.edu>
2015-11-30 14:38:24 -0500

    fixed it so that text and timestamps are stored then written to files

--- transcription/transcript_parsing.py
+++ transcription/transcript_parsing.py
@@ -2,38 +2,40 @@ import os
 # from audiosamples import mp3s
 
 curr_file = 'dracula_transcription.txt'
-out_file_trns = 'only_transcript.txt'
-out_file_time = 'only_timestamp.txt'
+output_file_trns = 'only_transcript.txt'
+output_file_time = 'only_timestamp.txt'
 
 
 def scrape_text(in_file, out_file):
-	lines = []
+	transcript_lines = []
 	with open(in_file, "r") as f:
-	    for line in curr_file:
-	    	if not any(char.isdigit() for char in line):
-	    		# print(line)
-	    		# maybe I need to write to the output file here, instead of in another function?
-	    		# line.append
-	    		return lines
-
+	    for line in f:
+	        if not any(char.isdigit() for char in line):
+	            transcript_lines.append(line)
+	return transcript_lines
 
 def scrape_timestamps(in_file, out_file):
-    lines = []
+    timestamp_lines = []
     with open(in_file, "r") as f:
         for line in f:
             if any(char.isdigit() for char in line):
-                # print(line)
-                return lines
-                # open(out_file_time, "w")
-                # out_file_time.append(line)
-
+                timestamp_lines.append(line)
+    return timestamp_lines
 
 
 def store_timestamps(timestamps, out):
-	for lines in timestamps:
-		with open(out_file_time, "w") as f:
-			f.write(lines)
+	with open(output_file_time, "w") as f:
+	    for timestamp_lines in timestamps:
+	    	f.write(timestamp_lines)
 
-scrape_text(curr_file, out_file_trns)
-scrape_timestamps(curr_file, out_file_time)
-store_timestamps(scrape_timestamps, out_file_time)
+def store_text(transcript, out):
+	with open(output_file_trns, "w") as f:
+		for transcript_lines in transcript:
+			f.write(transcript_lines)
+
+
+text_lines = scrape_text(curr_file, output_file_trns)
+time_lines = scrape_timestamps(curr_file, output_file_time)
+
+store_text(text_lines, output_file_trns)
+store_timestamps(time_lines, output_file_time)
