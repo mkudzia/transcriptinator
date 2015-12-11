43dcc45c2ae0105fe19d2099f944f4e8495e2610
Megan Kudzia <kudzia@mail.lib.msu.edu>
2015-12-11 12:10:33 -0500

    added some code, pseudocode for xml_chunks function; fixed for-loop spacing

--- transcription/XML_transcript_magic.py
+++ transcription/XML_transcript_magic.py
@@ -1,5 +1,6 @@
 # transcription/XML_transcript_magic.py
 
+
 import os
 
 folder = 'mp3s'
@@ -26,15 +27,22 @@ def insert_disclaimer(in_file):
 
 # this function is to divide the transcript into 6-word chunks to make reading and scrolling easier
 def xml_chunks(in_file):
+	with open(in_file, "r") as original:
+		data = original.read()
+	with open(in_file, "w") as chunked:
+		# rewind? index[0]? tell python to start at the beginning of the transcription text
+		# count six lines
+		# insert newline \n
+		# repeat every six lines
 
-	for files in os.listdir(folder):
-		input_xml = os.path.join(folder, files)
-		if '.xml' in files:
-			start_filename = os.path.splitext(files)[0]
+for files in os.listdir(folder):
+	input_xml = os.path.join(folder, files)
+	if '.xml' in files:
+		start_filename = os.path.splitext(files)[0]
 
-			xml_output = os.path.join(folder, start_filename + '_transcript.xml')
+		xml_output = os.path.join(folder, start_filename + '_transcript.xml')
 
-			insert_xml_start() # what args does this need?
+		insert_xml_start() # what args does this need?
 
-		else:
-			pass
+	else:
+		pass
