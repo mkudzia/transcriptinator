1e753ddf6d28724af8f0a34493e46aae9d6a1086
Megan Kudzia <kudzia@mail.lib.msu.edu>
2015-12-11 12:21:49 -0500

    added comments for clarity

--- transcription/transcript_parsing.py
+++ transcription/transcript_parsing.py
@@ -6,8 +6,7 @@ output_file_trns = 'only_transcript.txt'
 output_file_time = 'only_timestamp.txt'
 folder = 'mp3s'
 
-# each file needs a disclaimer regarding variable transcript quality
-
+# finds all the text-only lines from an audiogrep output file and append them into a list that gets returned
 def scrape_text(in_file):
 	transcript_lines = []
 	with open(in_file, "r") as f:
@@ -16,6 +15,7 @@ def scrape_text(in_file):
 	            transcript_lines.append(line)
 	return transcript_lines
 
+# finds all the lines with timestamp info from an audiogrep output file and append them into a list that gets returned
 def scrape_timestamps(in_file):
     timestamp_lines = []
     with open(in_file, "r") as f:
@@ -25,22 +25,25 @@ def scrape_timestamps(in_file):
     return timestamp_lines
 
 
+# writes the text returned from scrape_text to a file
 def store_text(transcript, out):
 	with open(out, "w") as f:
 		for transcript_lines in transcript:
 			f.write(transcript_lines)
 
-
+# writes the timestamp lines returned from scrape_timestamps to a text file
 def store_timestamps(timestamps, out):
 	with open(out, "w") as f:
 	    for timestamp_lines in timestamps:
 	    	f.write(timestamp_lines)
 
+# writes the timestamp lines returned from scrape_timestamps to an xml file
 def store_xml(timestamps, out):
 	with open(out, "w") as f:
 		for timestamp_lines in timestamps:
 			f.write(timestamp_lines)
 
+# loops through files in a directory; looks for transcription text files, and executes writing the new files
 for files in os.listdir(folder):
 	input_file = os.path.join(folder, files)
 	if 'transcription.txt' in files:
