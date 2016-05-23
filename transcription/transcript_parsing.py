588f3f2ce62af5448e8ea0ceb8b6c6fbe70af0ad
Megan Kudzia <kudzia@mail.lib.msu.edu>
2016-05-23 09:06:56 -0400

    eulfedora ingest version

--- transcription/transcript_parsing.py
+++ transcription/transcript_parsing.py
@@ -4,13 +4,12 @@ import os
 import shutil
 from lxml import etree as etree
 import re
+from datetime import timedelta
 
-output_file_trns = 'only_transcript.txt'
-output_file_time = 'only_timestamp.txt'
 # this is the folder you're keeping your source audio files in
-folder = 'mp3s'
-# this is the whole file path to the mp3 folder
-path = '/path/to/your/mp3s'
+folder = 'WOSL-Ingest'
+# this is the whole file path to the mp3 folder 
+path = '/VOLUMES/fedcom_ingest/WOSL-Ingest/'
 
 
 def create_cues(root, beginning, ending, transcript_text):
@@ -18,7 +17,6 @@ def create_cues(root, beginning, ending, transcript_text):
     start = etree.SubElement(cue, 'start')
     end = etree.SubElement(cue, 'end')
     transcript = etree.SubElement(cue, 'transcript')
-    speaker = etree.SubElement(cue, 'Different Speaker')
 
     start.text = str(beginning)
     end.text = str(ending)
@@ -27,10 +25,6 @@ def create_cues(root, beginning, ending, transcript_text):
 
 def create_root(in_file):
     root = etree.Element('cues')
-    disclaimer = etree.SubElement(root, 'disclaimer')
-    disclaimer.text = ('These transcripts were created by a software program; '
-                       'we make no guarantees as to the quality of the '
-                       'output. We know some of the words are incorrect.')
     return root
 
 
@@ -43,38 +37,37 @@ def file_names(folder):
 
 
 def iterator(in_file):
-    root = etree.Element('cues')
+    root = create_root(in_file)
     count = 0
     start_time = 0
-    # end_time = 0
-    # file_name = os.path.basename(in_file)
     beginsplit = os.path.splitext(in_file)[0]
     basefile = beginsplit.split('.')[0]
     out_file = basefile + '_transcript.xml'
+    vtt_out_file = basefile + '_vtt.vtt'
     with open(in_file) as transcriptfile:
+        # start a new cue string
+        cue_string = 'WEBVTT \n\n'
         for line in transcriptfile:
             if line[0].isalpha():
-                # print(line)
                 split_vals = line.split()
-                # for the first word
                 if count == 0:
-                    # start a new list of words
                     transcript_words = []
                     # grab the start time of the first word
                     start_time = (split_vals[1])
                     # save the first word to the list
                     transcript_words.append(split_vals[0])
                     count += 1
-                # for the sixth word
+
                 elif count == 6:
-                    # grab the stop time of the last word
                     end_time = (split_vals[2])
-                    # save the last word to the list
                     transcript_words.append(split_vals[0])
-                    # assemble the cue
+                    transcript_text = " ".join(transcript_words) 
                     create_cues(
-                        root, start_time, end_time, " ".join(transcript_words))
+                        root, start_time, end_time, transcript_text)
+
+                    cue_string += generate_caption(start_time, end_time, transcript_text)
                     count = 0  # reset the count
+
                 else:
                     # save the intervening words to the list
                     transcript_words.append(split_vals[0])
@@ -83,48 +76,71 @@ def iterator(in_file):
     tree = etree.ElementTree(root)
     tree.write(out_file, pretty_print=True,
                xml_declaration=True, encoding='UTF-8')
+    # print(vtt_out_file)
+    write_vtt(cue_string, vtt_out_file)
+
+def generate_caption(start, end, text):
+    """Create vtt-formatted caption.
+
+    args:
+        start(str): time in seconds e.g. 1.534
+        end(str): time in seconds e.g. 5.342
+        text(str): full text of caption
+    """
+    start_time = update_time_format(start)
+    end_time = update_time_format(end)
+    caption_text = remove_numbers(text)
+    return "{0} --> {1}\n{2}\n\n".format(start_time, end_time, caption_text)
+
+def update_time_format(time):
+    """Change time format from s.ss to hh:mm:ss.ss.
+
+    args:
+        time(str): time in s.ss format
+    """
+    total_seconds = float(time)
+    milliseconds = time.split(".")[1]
+    hours, remainder = divmod(total_seconds, 60*60)
+    minutes, seconds = divmod(remainder, 60)
+
+    hours_format = str(int(hours)).zfill(2)
+    minutes_format = str(int(minutes)).zfill(2)
+    seconds_format = str(round(seconds, 3)).zfill(6)
+    return "{0}:{1}:{2}".format(hours_format, minutes_format, seconds_format)
+
+
+def write_vtt(cue_string, out):
+    with open(out, "w") as f:
+        # print(cue_string, "in write_vtt")
+        f.write(cue_string)
 
 
 def make_folders(path):
-    for files in os.listdir(path):
-        if not files.startswith('.'):
-            start_splitting = os.path.splitext(files)[0]
-            new_folder = start_splitting.split('.')[0]
-            file_loc = os.path.join(path, files)
-            derivs_folder = path + new_folder
-            if os.path.isdir(derivs_folder):
-                pass
+    file_count = 0
+    for dirpath, dirnames, filenames in os.walk(path):
+        for f in filenames:
+            input_file = os.path.join(dirpath, f)
+            if 'transcription.txt' in f:
+                file_count += 1
+                print("{0}. Processing file: {1}".format(file_count, f))
+                filestart = os.path.splitext(f)[0]
+
+                transcript_output = os.path.join(
+                    dirpath, filestart + '_only_transcript.txt')
+                timestamp_output = os.path.join(
+                    dirpath, filestart + '_only_timestamp.txt')
+                timestamp_xml_out = os.path.join(
+                    dirpath, filestart + '_only_timestamp.xml')
+
+                text_lines = scrape_text(input_file)
+                time_lines = scrape_timestamps(input_file)
+
+                store_text(text_lines, transcript_output)
+                store_timestamps(time_lines, timestamp_output)
+                iterator(timestamp_output)
             else:
-                os.makedirs(derivs_folder)
-
-            if files.endswith('.mp3'):
-                shutil.copy2(file_loc, derivs_folder)
-            elif files.endswith('.txt'):
-                shutil.copy(file_loc, derivs_folder)
-            else:
                 pass
-
-            for files in os.listdir(derivs_folder):
-                input_file = os.path.join(derivs_folder, files)
-                if 'transcription.txt' in files:
-                    filestart = os.path.splitext(files)[0]
-
-                    transcript_output = os.path.join(
-                        derivs_folder, filestart + '_only_transcript.txt')
-                    timestamp_output = os.path.join(
-                        derivs_folder, filestart + '_only_timestamp.txt')
-                    timestamp_xml_out = os.path.join(
-                        derivs_folder, filestart + '_only_timestamp.xml')
-
-                    text_lines = scrape_text(input_file)
-                    time_lines = scrape_timestamps(input_file)
-
-                    store_text(text_lines, transcript_output)
-                    store_timestamps(time_lines, timestamp_output)
-                    store_xml(time_lines, timestamp_xml_out)
-                else:
-                    pass
-            file_names(derivs_folder)
+        
 
 
 def remove_numbers(text_string):
@@ -164,12 +180,6 @@ def store_timestamps(timestamps, out):
         for timestamp_lines in timestamps:
             f.write(timestamp_lines)
 
-
-# writes the timestamp lines returned from scrape_timestamps to an xml file
-def store_xml(timestamps, out):
-    with open(out, "w") as f:
-        for timestamp_lines in timestamps:
-            f.write(timestamp_lines)
 
 
 make_folders(path)
